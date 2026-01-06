53cc3bd0e1fe63709bc0b5714e5b88fb850655df
furkan <furkangurbuz5@gmail.com>
2026-01-06 12:57:10 +0100

    Format table output to currency

--- src/app/investment-result/investment-result.component.ts
+++ src/app/investment-result/investment-result.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input} from '@angular/core';
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-investment-result',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-result.component.html',
   styleUrl: './investment-result.component.css'
 })
